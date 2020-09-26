befc322ae59a2acd34d92a9d7cfa258012c524ef
omarmagdy <omarmagdy99860@gmil.com>
2020-09-26 12:51:58 +0200

    omar

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+
+var options = {
+  strings: ['npm install^1000\n `installing components...` ^1000\n `Fetching from source...`'],
+  typeSpeed: 40,
+  smartBackspace: true,
+  loop:true,
+  cursorChar: '',
+};
+
+
+var typed = new Typed('.sec-type', options);
+
+
+
+
+
+
+
+})
